2759a3b520737132f9eea80429507ff6b7fdd166
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:36:39 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -2,20 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [name,setname] = useState("")
+  const [greeting,setGreeting]=useState("")
+const handleChange = (e) =>
+{
+setname(e.target.value)
+}
+
+const handleSubmit = (event) =>
+{
+  event.preventDefault()
+  setGreeting(`Hello, ${name}!`)
+}
+
   return (
     <div className="App">
      <h2>Hey!!! Greeting</h2>
-     <form>
+     <form onSubmit={handleSubmit}>
       <label>
         Enter your name:
         <input
         type="text"
-        onChange={ha}
+        value={name}
+        onChange={handleChange}
         />
       </label>
       </form>
     </div>
   );
 }
-
 export default App;
