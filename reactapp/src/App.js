68715b4a005a1146c9bb10044eebe3a7078d34b3
Iamneo <techadmin@iamneo.ai>
2023-09-27 06:31:39 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div className="App">
      <h2>Hey!!! Greeting</h2>
+     <form>
+      <label>
+        Enter your name:
+        <input
+        type="text"
+        onChange={ha}
+        />
+      </label>
+      </form>
     </div>
   );
 }
